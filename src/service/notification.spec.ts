d8db5f5567970e41abd151f50817bfa4a9b9e471
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-09-07 00:52:18 +0000

    # SSKTS-533 - add specs.

--- src/service/notification.spec.ts
+++ src/service/notification.spec.ts
@@ -5,6 +5,7 @@
 
 // tslint:disable-next-line:no-require-imports
 import sgMail = require('@sendgrid/mail');
+import { ACCEPTED, BAD_REQUEST, OK } from 'http-status';
 import * as nock from 'nock';
 import * as assert from 'power-assert';
 import * as sinon from 'sinon';
@@ -27,13 +28,30 @@ describe('sendEmail()', () => {
             group: 'group',
             data: <any>{}
         };
-        const sendResponse = [{ statusCode: 202 }];
+        const sendResponse = [{ statusCode: ACCEPTED }];
 
         sandbox.mock(sgMail).expects('send').once().returns(Promise.resolve(sendResponse));
 
         const result = await sskts.service.notification.sendEmail(notification)();
 
         assert.equal(result, undefined);
+        sandbox.verify();
+    });
+
+    it('SendGridAPIのステータスコードがACCEPTEDでなｋれば、エラーになるはず', async () => {
+        const notification = {
+            id: 'id',
+            group: 'group',
+            data: <any>{}
+        };
+        const sendResponse = [{ statusCode: BAD_REQUEST }];
+
+        sandbox.mock(sgMail).expects('send').once().returns(Promise.resolve(sendResponse));
+
+        const sendEmailError = await sskts.service.notification.sendEmail(notification)()
+            .catch((err) => err);
+
+        assert(sendEmailError instanceof Error);
         sandbox.verify();
     });
 });
@@ -44,12 +62,46 @@ describe('report2developers()', () => {
     });
 
     it('LINE Notifyの状態が正常であれば、エラーにならないはず', async () => {
-        // tslint:disable-next-line:no-magic-numbers
-        const scope = nock('https://notify-api.line.me').post('/api/notify').reply(200);
+        const scope = nock('https://notify-api.line.me').post('/api/notify').reply(OK);
+        const imageThumbnail = 'https://example.com';
+        const imageFullsize = 'https://example.com';
 
-        const result = await sskts.service.notification.report2developers('', '')();
+        const result = await sskts.service.notification.report2developers('', '', imageThumbnail, imageFullsize)();
 
         assert.equal(result, undefined);
         assert(scope.isDone());
+    });
+
+    it('LINE Notifyの状態が正常でなければ、エラーになるはず', async () => {
+        const scope = nock('https://notify-api.line.me').post('/api/notify').reply(BAD_REQUEST);
+
+        const reportError = await sskts.service.notification.report2developers('', '')()
+            .catch((err) => err);
+
+        assert(reportError instanceof Error);
+        assert(scope.isDone());
+    });
+
+    it('imageThumbnailがURLでなければ、エラーになるはず', async () => {
+        const scope = nock('https://notify-api.line.me').post('/api/notify').reply(OK);
+        const imageThumbnail = 'invalidUrl';
+
+        const reportError = await sskts.service.notification.report2developers('', '', imageThumbnail)()
+            .catch((err) => err);
+
+        assert(reportError instanceof sskts.factory.errors.Argument);
+        assert(!scope.isDone());
+    });
+
+    it('imageFullsizeがURLでなければ、エラーになるはず', async () => {
+        const scope = nock('https://notify-api.line.me').post('/api/notify').reply(OK);
+        const imageThumbnail = 'https://example.com';
+        const imageFullsize = 'invalidUrl';
+
+        const reportError = await sskts.service.notification.report2developers('', '', imageThumbnail, imageFullsize)()
+            .catch((err) => err);
+
+        assert(reportError instanceof sskts.factory.errors.Argument);
+        assert(!scope.isDone());
     });
 });
