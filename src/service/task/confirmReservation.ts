0776927f707bf8526701b68c8e8f1c7da259536e
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-02-05 07:04:21 +0000

    # SSKTS-1141 ### Added - 予約確定タスクを追加

--- src/service/task/confirmReservation.ts
+++ src/service/task/confirmReservation.ts
@@ -1,9 +1,10 @@
+
+import { service } from '@cinerino/domain';
+
 import { IConnectionSettings, IOperation } from '../task';
 
 import * as factory from '../../factory';
 import { MongoRepository as ActionRepo } from '../../repo/action';
-
-import * as ReservationService from '../reservation';
 
 /**
  * タスク実行関数
@@ -12,7 +13,7 @@ export function call(data: factory.task.IData<factory.taskName.ConfirmReservatio
     return async (settings: IConnectionSettings) => {
         const actionRepo = new ActionRepo(settings.connection);
 
-        await ReservationService.confirmReservation(<any>data)({
+        await service.reservation.confirmReservation(<any>data)({
             action: actionRepo,
             reserveService: <any>null //  シネマサンシャインではchevreを使用しないので、いったん問題ない
         });
